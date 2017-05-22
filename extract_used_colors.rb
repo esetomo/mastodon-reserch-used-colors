b05b3843d0ccc98bd3d81a506c655ad99c7819e9
Ichigo Mayo <esetomo@gmail.com>
2017-05-22 20:53:35 +0900

    extract base colors too.

--- extract_used_colors.rb
+++ extract_used_colors.rb
@@ -4,6 +4,6 @@ require 'pathname'
 targets = Pathname.glob('live/app/javascript/styles/*.scss') + Pathname.glob('live/app/assets/stylesheets/*.scss')
 
 target_contents = targets.map{|f| f.read}.join
-target_contents.scan(/(?<paren>(?:darken|lighten|rgba)\((?:[^()]|\g<paren>)*\))/).flatten.uniq.sort.each do |colorspec|
+target_contents.scan(/(?<paren>(?:darken|lighten|rgba)\((?:[^()]|\g<paren>)*\))|(?<var>\$color\d)|(?<hex>\#[\da-f]{6})/).flatten.compact.uniq.sort.each do |colorspec|
   print colorspec, "\n"
 end
